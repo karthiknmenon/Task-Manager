ef364a6a0447ea1e8d6e4f4c04e43f69ada8e2e4
mojojojo20 <menonk484@gmail.com>
2019-06-26 16:26:44 +0530

    fix search icon

---
+++ Task Manager/js/form.js
@@ -0,0 +1,37 @@
+// new task form
+$(document).ready(function() {
+    $(document).on('submit', '.new-form', function() {
+        var tDDate = new Date();
+        var tName = $('.new-form').find('input[name="taskName"]').val();
+        var tDesc = $('.new-form').find('input[name="taskDesc"]').val();
+        var tTags = $('.new-form').find('input[name="taskTags"]').val();
+        var tDDate = $('.new-form').find('input[name="taskDDate"]').val();
+        var tADate = $('.new-form').find('input[name="taskADate"]').val();
+        var tColor = $('.new-form').find('input[name="taskColor"]').val();
+        createTask(tName,tDesc,tTags,tDDate,tADate,tColor);
+        return false;
+   
+    });
+});
+// create new task
+function createTask(tName,tDesc,tTags,tDDate,tADate,tColor){
+    var card_element = '<li> <h3>'+tName+'</h3> <p id="disc">'+tDesc+'</p> <p id="date">'+tDDate+'</p> <img id="completed" src="/Task Manager/images/outline-done-24px.svg" onclick="color(this)" ondbclick="black(this)"> <img id="priority" src="/Task Manager/images/outline-star_border-24px (1).svg" onclick="color(this)" ondbclick="black(this)"> <img id="archive" src="/Task Manager/images/outline-archive-24px.svg" onclick="color(this)" ondbclick="black(this)"> </li><br>'
+    $("#search-re").append(card_element);
+}
+//search functiom
+function search() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("tSearch");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("search-re");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("h3")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "block";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
