90248c12d0861c1a4ddfdbf00e0225bc36844aaf
mojojojo20 <menonk484@gmail.com>
2019-06-26 11:00:47 +0530

    implement searching of tasks

---
+++ Task Manager/js/newTask.js
@@ -0,0 +1,9 @@
+
+// var task_name = 'Project Name';
+
+// const featured_task_element = '<p>'+task_name+'</p>';
+
+// const featured_task_element = '<div class="task-body__card--preview"> <span id="tag-color"></span> <h5>'+task_name+'</h5> <span id="sep"></span> <div class="task-body__card--preview_body"> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nostrum nesciunt.</p> <img src="/Task Manager/images/outline-calendar_today-24px.svg" id="card-calendar"> <p id="due-date">21/06/2019</p> <div class="icons"> <img id="completed" src="/Task Manager/images/outline-done-24px.svg" onclick="color(this)" ondbclick="black(this)"> <img id="priority" src="/Task Manager/images/outline-star_border-24px (1).svg" onclick="color(this)" ondbclick="black(this)"> <img id="archive" src="/Task Manager/images/outline-archive-24px.svg" onclick="color(this)" ondbclick="black(this)"> </div> </div> </div>';
+
+
+// werapper_container.append(featured_task_element);
