ce96531a908bbd7edba7acce5778d33ae987d431
mojojojo20 <menonk484@gmail.com>
2019-06-27 10:03:36 +0530

    tag-color

--- Task Manager/js/index.js
+++ Task Manager/js/index.js
@@ -7,6 +7,7 @@ function addTasks() {
 // to display new task form
 function reveal() {
     var x = document.getElementsByClassName('task-body--add');
+    $(".new-form")[0].reset();
     x[0].style.display = "inline";
 }
 // to exit new task form
@@ -35,4 +36,43 @@ function color(obj) {
 
 function black(obj) {
     obj.style.filter = "grayscale(1)";
+}
+
+// new task form
+$(document).ready(function() {
+    $(document).on('submit', '.new-form', function() {
+        var tDDate = new Date();
+        var tName = $('.new-form').find('input[name="taskName"]').val();
+        var tDesc = $('.new-form').find('input[name="taskDesc"]').val();
+        var tTags = $('.new-form').find('input[name="taskTags"]').val();
+        var tDDate = $('.new-form').find('input[name="taskDDate"]').val();
+        var tADate = $('.new-form').find('input[name="taskADate"]').val();
+        var tColor = $('.new-form').find('input[name="taskColor"]').val();
+        createTask(tName,tDesc,tTags,tDDate,tADate,tColor);
+        goback(); 
+        return false;
+    });
+});
+// create new task
+function createTask(tName,tDesc,tTags,tDDate,tADate,tColor){
+    var card_element = ' <div class="result-area"> <li> <div id="task-color" style="background-color:'+tColor+'"></div><h3>asdasd</h3> <p id="disc">'+tDesc+'</p> <p id="date">Due Date: '+tDDate+'</p> <img id="completed" class="sIcon" onclick="color(this)" ondbclick="black(this)" src="/Task Manager/images/outline-done-24px.svg"> <img id="priority" class="sIcon" onclick="color(this)" ondbclick="black(this)" src="/Task Manager/images/outline-star_border-24px (1).svg"> <img id="archive" class="sIcon" onclick="color(this)" ondbclick="black(this)" src="/Task Manager/images/outline-archive-24px.svg"> </li></div><br>'
+    $("#search-re").append(card_element);
+}
+//search functiom
+function search() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = $("#tSearch");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("search-re");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("h3")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            // opacity
+            li[i].style.display = "block";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
 }
